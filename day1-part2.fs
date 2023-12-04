34dceed91da15fc36489fdfcd3675aea69b21a10
Simon Nyvall <simon.k.nyvall@proton.me>
2023-12-04 14:38:33 +0100

    Day 1 part 2

--- day1-part2.fs
+++ day1-part2.fs
@@ -1,40 +1,68 @@
+module day1.part2
 
 open System
 open System.IO
 open System.Text.RegularExpressions
 
-let dictmap = 
-  dict [
-    "one", "1"; "two", "2"; "three", "3"; "four", "4"; "five", "5"; "six", "6"; "seven", "7"; "eight", "8"; "nine", "9";
-  ]
+let readtextfile = File.ReadAllLines("input/input1.txt")
 
-let readtextfile = 
-  File.ReadAllLines("input/input1.txt")
 
-let replaceWords (line: string) =
-  let regex = Regex("one|two|three|four|five|six|seven|eight|nine")
-  regex.Replace(line, fun m -> dictmap.[m.Value])
 
-let findDigits (line: string) =
-  let digits = line.ToCharArray() |> Array.filter Char.IsDigit
-  match digits.Length with
-  | 0 -> None
-  | 1 -> Some(digits.[0], digits.[0])
-  | _ -> Some(digits.[0], digits.[digits.Length - 1])
+let findTextDigites (line: string) =
+    let regex = Regex("(?=(one|two|three|four|five|six|seven|eight|nine|[1-9]))")
+    let matches = regex.Matches line
+    let results = ResizeArray<string>()
 
-let parseLine (line: string) =
-  match findDigits line with
-  | Some(firstDigit, lastDigit) -> 
-      int (string firstDigit) * 10 + int (string lastDigit)
-  | None -> 0
+    for isMatch in matches do
+        if isMatch.Groups.[1].Success then
+            results.Add(isMatch.Groups.[1].Value)
 
-let parseAndSum fileLines =
-  fileLines |> Array.map parseLine |> Array.sum
+    results.ToArray()
 
-let totalSum = readtextfile |> Array.map replaceWords |> parseAndSum
-totalSum
+let textDigitMap =
+    dict [ "one", "1"
+           "two", "2"
+           "three", "3"
+           "four", "4"
+           "five", "5"
+           "six", "6"
+           "seven", "7"
+           "eight", "8"
+           "nine", "9"
+           "1", "1"
+           "2", "2"
+           "3", "3"
+           "4", "4"
+           "5", "5"
+           "6", "6"
+           "7", "7"
+           "8", "8"
+           "9", "9" ]
+
+let convertTextToDigits (arr: string []) =
+    arr |> Array.map (fun x -> textDigitMap.[x])
+
+
+let findFirstAndLastDigits (digits: string []) =
+
+    match digits.Length with
+    | 0 -> None
+    | 1 -> Some(digits.[0], digits.[0])
+    | _ -> Some(digits.[0], digits.[digits.Length - 1])
+
+
+let result (arr: Option<string * string> []) =
+    arr
+    |> Array.filter (fun x -> x.IsSome)
+    |> Array.map (fun x -> x.Value)
+    |> Array.map (fun (x, y) -> x + y)
+    |> Array.map int
+    |> Array.sum
+
 
 
 readtextfile
-|> Array.map replaceWords
-
+|> Array.map findTextDigites
+|> Array.map convertTextToDigits
+|> Array.map findFirstAndLastDigits
+|> result
